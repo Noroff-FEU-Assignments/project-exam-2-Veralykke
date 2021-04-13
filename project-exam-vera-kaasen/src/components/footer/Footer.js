c1f9a8f9306d6764242dc0b6bbb38a5f98169508
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2021-04-13 13:55:45 +0200

    icons

--- project-exam-vera-kaasen/src/components/footer/Footer.js
+++ project-exam-vera-kaasen/src/components/footer/Footer.js
@@ -9,29 +9,19 @@ faTwitter
 const Footer = () => {
   return (
     <>
-    <div classNmae="social-container">
-      <a
-      href="https://www.facebook.com/"
-      className="facebook-social"
-      >
-        <FontAwesomeIcon icon= {faFacebook} size="2x" />
+    <div className="social-container">
+      <a href="https://www.facebook.com/"
+      className="facebook-social"><FontAwesomeIcon icon= {faFacebook} size="2x" />
       </a>
 
-      <a
-      href="https://www.instagram.com/"
-      className="instagram-social"
-      >
-        <FontAwesomeIcon icon= {faInstagram} size="2x" />
+      <a href="https://www.instagram.com/"
+      className="instagram-social"><FontAwesomeIcon icon= {faInstagram} size="2x" />
       </a>
 
-      <a
-      href="https://twitter.com/"
-      className="twitter-social"
-      >
-        <FontAwesomeIcon icon= {faTwitter} size="2x" />
+      <a href="https://twitter.com/"
+      className="twitter-social"><FontAwesomeIcon icon= {faTwitter} size="2x" />
       </a>
     </div>
-
 
       <Nav defaultActiveKey="/home" className="flex-column">
         <div className="row">
