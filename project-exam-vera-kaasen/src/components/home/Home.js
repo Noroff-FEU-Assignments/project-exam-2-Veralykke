db895a7bf1d9e0c653d3831f5a16032b2528afe4
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2021-04-15 15:34:13 +0200

    added background

--- project-exam-vera-kaasen/src/components/home/Home.js
+++ project-exam-vera-kaasen/src/components/home/Home.js
@@ -1,40 +1,21 @@
 import Heading from "../layout/Heading";
-import Image from 'react-bootstrap/Image'
+//import bergen from "./images/bergen.jpg";
 //import Jumbotron from 'react-bootstrap/Jumbotron'
 //import Container from 'react-bootstrap/Container'
+import { FormControl, Form, Button } from "react-bootstrap";
+
 
 export default function Home() {
   return ( 
     <>
-<Image src="holder.js/100px250" fluid />
+     <Heading title="We bring bergen to you" />
+    <div id="bg-home">
+      <Form inline>
+    <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
+    <Button type="submit">Submit</Button>
+  </Form>
+    </div>
   </>
-  )
-  }
+  );
+}
 
-
-
-
-//JUMBOTRON
-
-/*export default function Home() {
-  return ( 
-    <>
-  <Jumbotron fluid>
-  <Container>
-  <Heading title="Home" />
-     <img src="/" alt="bg-home"/>
-     <div class="input-group rounded">
-  <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
-    aria-describedby="search-addon" />
-  <span class="input-group-text border-0" id="search-addon">
-    <i class="fas fa-search"></i>
-  </span>
-  </div>
-  </Container>
-  </Jumbotron>
-  </>
-  )
-  }
-*/
-
-//JUMBOTRON
