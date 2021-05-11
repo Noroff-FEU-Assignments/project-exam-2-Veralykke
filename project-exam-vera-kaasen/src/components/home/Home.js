23056398029aaabe3501d45c3baef62fc6840fc6
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2021-05-11 12:58:26 +0200

    home

--- project-exam-vera-kaasen/src/components/home/Home.js
+++ project-exam-vera-kaasen/src/components/home/Home.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <>
       <img src="/images/bergen.jpg" className="image-home"></img>
-    <Container>
+  
           <p className="txt-home">We bring</p>
           <h1 className="h1-home">Bergen</h1>
           <p className="txt-home-two">To you</p>
           <Search />
-      </Container>
+    
     </>
   );
 }
