81b28504dd9ca2ad129ead38676272b54d3d03cb
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2021-05-26 12:25:38 +0200

    home-styling

--- project-exam-vera-kaasen/src/components/home/Home.js
+++ project-exam-vera-kaasen/src/components/home/Home.js
@@ -5,10 +5,11 @@ export default function Home() {
     <>
       <div
         className="image-home"
-        alt="Picture of Bergen city, Credit: User of Unsplash: ZHANG Shaoqi"
-      >
+        alt="Picture of Bergen city, Credit: User of Unsplash: ZHANG Shaoqi">
+        <div className="text-home">
         <h3 className="h3-home">HOLIDAZE</h3>
         <h1 className="h1-home">Hotels in Bergen</h1>
+        </div>
         <Search className="mr-sm-2" />
       </div>
     </>
